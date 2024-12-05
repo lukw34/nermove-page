542b06edc1cab58255dc7f9539c2df7ea6fc7fc5
Łukasz Walewski <lukw34@gmail.com>
2024-12-05 22:58:25 +0100

    Meeting (#1)
    Changes after meeting

--- src/components/MainPicture/MainPicture.tsx
+++ src/components/MainPicture/MainPicture.tsx
@@ -6,17 +6,12 @@ import i18next from 'i18next';
 
 const MainPicture = () => (
   <section className={styles.mainImageContainer}>
-    <img 
-      alt="Burger track"
-      className={styles.mainImage} 
-      src="https://innovokitchen.com/wp-content/uploads/2022/01/Innovo-Food-Truck-13.jpg"
-    />
     <div className={styles.mainImageHeaderContainer}>
-      <h2>Przyczepy od {i18next.t('priceWithCurrency',{ val: 20000 })}</h2>
-      <div className={styles.mainImageButtonContainer }>
-        <LinkButton title="Skontaktuj sie z nami !" to="/contact"/>
-        <LinkButton title="Zobacz nasze realizacje !" to="/gallery"/>
-      </div>
+      <h2>PRZYCZEPY OD {i18next.t('priceWithCurrency',{ val: 20000 })}</h2>
+    </div>
+    <div className={styles.mainImageButtonContainer }>
+      <LinkButton title="Skontaktuj sie z nami !" to="/contact"/>
+      <LinkButton title="Zobacz nasze realizacje !" to="/gallery"/>
     </div>
   </section>
 );
