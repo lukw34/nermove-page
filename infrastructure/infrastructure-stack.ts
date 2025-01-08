d4e9b39de80dc24d1f006bbba273e6ef82cd1d6d
lukw34 <lukw34@gmail.com>
2025-01-08 15:53:37 +0100

    Add cloud front to infrastructure

--- infrastructure/infrastructure-stack.ts
+++ infrastructure/infrastructure-stack.ts
@@ -3,30 +3,41 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3Deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as path from 'path';
+import { 
+  AllowedMethods,
+  Distribution,
+  OriginAccessIdentity,
+  OriginProtocolPolicy, 
+} from 'aws-cdk-lib/aws-cloudfront';
+import { HttpOrigin, S3BucketOrigin, S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     const bucket = new s3.Bucket(this, 'NeromovePageBucket', {
       bucketName:  'neromove-page',
-      publicReadAccess: true,
+      publicReadAccess: false,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
-      blockPublicAccess: new s3.BlockPublicAccess({
-        blockPublicAcls: false,
-        blockPublicPolicy: false,
-        ignorePublicAcls: false,
-        restrictPublicBuckets: false
-      }),
       versioned: true,
     });
-
+    
     new s3Deploy.BucketDeployment(this, 'NeromovePageBucketDeployment', {
       sources: [
         s3Deploy.Source.asset(path.join(__dirname, '../build')),
       ],
       destinationBucket: bucket
     });
+    
+    const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
+    bucket.grantRead(originAccessIdentity);
+
+    new Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: S3BucketOrigin.withOriginAccessControl(bucket)
+      }
+    });
+    
   }
 }
